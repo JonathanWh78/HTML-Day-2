02719d2911b066431d70c7ee81e6391381751972
jonathanwh <jonathanwh78@gmail.com>
2022-02-01 12:40:14 +0000

    qa

---
+++ QAChart2Ex4.js
@@ -0,0 +1,26 @@
+let A=100;
+
+while (A <= 200) 
+{
+  if (A % 2 == 0) 
+  {
+    console.log(`The Number: ${A} is divisable by 2`);  
+  } 
+  else 
+  {
+    console.log("Is Not Divisable by 2");
+  }
+  A++;
+}
+
+for (let A =100; A<=200; A++)
+{
+    if (A % 2 == 0)
+    {
+        console.log("this is a for loop"); 
+    }
+    else
+    {
+        console.log("this is the else");
+    }
+}
---
+++ QAExercise4.js
@@ -0,0 +1,13 @@
+let A = 100;
+
+while (A<200)
+{
+    console.log(A);
+    A++;
+}
+
+for(let A = 100; A<200; A++)
+{
+    console.log("for loop: "+A);
+    A++;
+}
