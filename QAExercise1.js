48507819950c671d587419ff6838fe2c1ff84d9f
jonathanwh <jonathanwh78@gmail.com>
2022-02-01 10:43:36 +0000

    working now

--- QAExercise1.js
+++ QAExercise1.js
@@ -1,6 +1,13 @@
-'use strict'
+'use strict';
 
-console.info("I am an info")
-console.log("I am an info")
-console.warn("I am an info")
-console.error("I am an info")
+console.info("I am an info");
+console.log("I am an info");
+console.warn("I am an info");
+console.error("I am an info");
+
+let make = "Audi";
+let model = "A5";
+
+console.log("My Favourite Car Is An: " + make + model);
+
+console.log('%c' + 'My Favourite Car Is An: ' + make + model, 'color:orange; font-family:fantasy; font-style: Bold; background-color: black;padding: 10px";');
